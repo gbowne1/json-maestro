d0631c0929ff1d5a456229375aa42ecde2e796b4
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-10-19 18:14:41 +0200

    update test file and remove print statement

--- tests/test_cleaners.py
+++ tests/test_cleaners.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 
 from jsonmaestro import remove_comments
@@ -6,21 +5,22 @@ from jsonmaestro import remove_comments
 
 @pytest.mark.parametrize("file_name", [
     "jsonc.json", "commented.json", "commented_with_duplicate.json",
-    "bc_symbols.json", "ds_symbols.json", "mixed_comment_symbols.json", "package-lock-data.json"
+    "bc_symbols.json", "ds_symbols.json", "mixed_comment_symbols.json",
+    "package-lock-data.json"
 ])
 def test_remove_comments(file_name):
 	with open(f"data/{file_name}", "r") as file:
 		data = remove_comments(file.read())
-		print(data)
 
 		if not file_name in [
-		    "bc_symbols.json", "ds_symbols.json", "mixed_comment_symbols.json"
+		    "bc_symbols.json", "ds_symbols.json", "mixed_comment_symbols.json",
+		    "package-lock-data.json"
 		]:
 			assert "//" not in data
 			assert "/*" not in data
 			assert "*/" not in data
 		else:
-			if file_name == "ds_symbols.json":
+			if file_name == "ds_symbols.json" or file_name == "package-lock-data.json":
 				assert "//" in data
 			elif file_name == "bc_symbols.json":
 				assert "/*" in data
