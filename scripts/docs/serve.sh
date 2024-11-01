9393b9f8432af36697541851e2c29fdefcc2ffee
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-01 13:38:55 +0100

    update serve script to pull all relevant files from root

--- scripts/docs/serve.sh
+++ scripts/docs/serve.sh
@@ -6,7 +6,13 @@ set -x
 
 rm -rf book
 rm -rf docs/README.md
+rm -rf docs/CONTRIBUTING.md
+rm -rf docs/CODE_OF_CONDUCT.md
+rm -rf docs/CHANGELOG.md
 
 cp README.md docs/README.md
+cp CONTRIBUTING.md docs/CONTRIBUTING.md
+cp CODE_OF_CONDUCT.md docs/CODE_OF_CONDUCT.md
+cp CHANGELOG.md docs/CHANGELOG.md
 
 mdbook serve
