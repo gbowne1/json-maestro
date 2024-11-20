3ba2ed5c4962d85a52d6501da39121d8a0977d55
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-20 20:16:02 +0100

    cleanup pyi file for logger module

--- src/jsonmaestro/logger.pyi
+++ src/jsonmaestro/logger.pyi
@@ -1,48 +1,49 @@
 from typing import Any, NoReturn
 
-NO_LEVEL: int
-DEBUG_LEVEL: int
-INFO_LEVEL: int
-WARN_LEVEL: int
-ERROR_LEVEL: int
-FATAL_LEVEL: int
 
-RESET_COLOR: str
-DEBUG_COLOR: str
-INFO_COLOR: str
-WARN_COLOR: str
-ERROR_COLOR: str
-FATAL_COLOR: str
-
-
-# Functions
-def logger_print(level: int, message: str) -> None:
+def log(message: str, **kwargs: Any) -> None:
 	...
 
 
-def construct_message(message: str, **kwargs: dict[Any, Any]) -> str:
+def logf(format_str: str, *args: Any) -> None:
 	...
 
 
-def log(message: str, **kwargs: dict[Any, Any]) -> None:
+def debug(message: str, **kwargs: Any) -> None:
 	...
 
 
-def debug(message: str, **kwargs: dict[Any, Any]) -> None:
+def debugf(format_str: str, *args: Any) -> None:
 	...
 
 
-def info(message: str, **kwargs: dict[Any, Any]) -> None:
+def info(message: str, **kwargs: Any) -> None:
 	...
 
 
-def warn(message: str, **kwargs: dict[Any, Any]) -> None:
+def infof(format_str: str, *args: Any) -> None:
 	...
 
 
-def error(message: str, **kwargs: dict[Any, Any]) -> None:
+def warn(message: str, **kwargs: Any) -> None:
 	...
 
 
-def fatal(message: str, **kwargs: dict[Any, Any]) -> NoReturn:
+def warnf(format_str: str, *args: Any) -> None:
+	...
+
+
+def error(message: str, **kwargs: Any) -> None:
+	...
+
+
+def errorf(format_str: str, *args: Any) -> None:
+	...
+
+
+def fatal(message: str, **kwargs: Any) -> NoReturn:
+	...
+
+
+def fatalf(format_str: str, *args: Any) -> NoReturn:
 	...
