ce39df96bd785dda2beaefc4faef5ab76201518c
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-23 19:08:12 +0100

    cleanup load_as function

--- src/jsonmaestro/loaders.py
+++ src/jsonmaestro/loaders.py
@@ -1,3 +1,4 @@
+from io import TextIOWrapper
 import json
 import csv
 
@@ -22,6 +23,46 @@ class Loader():
 	def __init__(self, file_path: str):
 		self.file_path = file_path
 
+	def _open_file(self) -> TextIOWrapper:
+		"""
+		Open the file.
+		"""
+		return open(self.file_path, 'r', encoding='utf-8')
+
+	def _read_to_string(self) -> str:
+		"""
+		Read the file to a string.
+		"""
+		with open(self.file_path, 'r', encoding='utf-8') as file:
+			return file.read()
+
+	def _load_json(self, type: str) -> str:
+		"""
+		Load the file as JSON or JSONC.
+
+		Args:
+		type: The type of file to load. Can be either "jsonc" or "json".
+		"""
+		content = self._read_to_string()
+
+		if type == "jsonc":
+			content = remove_comments(content)
+
+		return content
+
+	def _load_csv(self) -> List[Dict[Union[str, Any], Union[str, Any]]]:
+		"""
+		Load the file as CSV.
+		"""
+		csvFile = csv.DictReader(self._open_file())
+
+		content: List[Dict[Union[str, Any], Union[str, Any]]] = []
+
+		for row in csvFile:
+			content.append(row)
+
+		return content
+
 	def load_as(
 	    self, format: str
 	) -> Union[Dict[str, Any], List[Dict[Union[str, Any], Union[str, Any]]]]:
@@ -39,29 +80,16 @@ class Loader():
 		LoaderValueError: If the file is not a valid JSON or JSONC file.
 		"""
 
-		with open(self.file_path, 'r', encoding='utf-8') as file:
-			content = file.read()
+		if format == "jsonc" or format == "json":
+			try:
+				return json.loads(self._load_json(format))
+			except json.JSONDecodeError:
+				raise LoaderFormatError()
+			except ValueError:
+				raise LoaderValueError()
 
-			if format == "jsonc":
-				content = remove_comments(content)
+		if format == "csv":
+			return self._load_csv()
 
-			if format == "jsonc" or format == "json":
-				try:
-					return json.loads(content)
-				except json.JSONDecodeError:
-					raise LoaderFormatError()
-				except ValueError:
-					raise LoaderValueError()
-
-			if format == "csv":
-				csvFile = csv.DictReader(file)
-
-				rcontent: List[Dict[Union[str, Any], Union[str, Any]]] = []
-
-				for row in csvFile:
-					rcontent.append(row)
-
-				return rcontent
-
-			else:
-				raise LoaderFormatError(f"Unsupported format: {format}")
+		else:
+			raise LoaderFormatError(f"Unsupported format: {format}")
