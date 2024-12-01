867f533135f718143d548209c26984208dfb1950
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-12-01 20:58:56 +0100

    add windows docs scripts

---
+++ scripts/docs/build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+COPY /Y CONTRIBUTING.md docs\CONTRIBUTING.md
+COPY /Y CODE_OF_CONDUCT.md docs\CODE_OF_CONDUCT.md
+COPY /Y CHANGELOG.md docs\CHANGELOG.md
+COPY /Y LICENSE docs\LICENSE
+
+mdbook build
---
+++ scripts/docs/build.ps1
@@ -0,0 +1,6 @@
+Copy-Item CONTRIBUTING.md docs/CONTRIBUTING.md
+Copy-Item CODE_OF_CONDUCT docs/CODE_OF_CONDUCT.md
+Copy-Item CHANGELOG.md docs/CHANGELOG.md
+Copy-Item LICENSE docs/LICENSE
+
+mdbook build
