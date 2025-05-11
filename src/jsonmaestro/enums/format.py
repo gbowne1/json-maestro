7f469e594ce9de5e916125d7be1947961b6ee09b
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-05-11 19:55:18 +0200

    add enum for file format

---
+++ src/jsonmaestro/enums/format.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from typing import List
+
+from jsonmaestro.errors import InvalidArgument
+
+
+class Fromat(Enum):
+    eNone = 0
+    eJson = 1
+    eJsonWithCommnets = 2
+
+    @classmethod
+    def count(cls) -> int:
+        return len(cls)
+
+    @classmethod
+    def max(cls) -> int:
+        return max(cls.values())
+
+    @classmethod
+    def names(cls) -> List[str]:
+        return [item.name for item in cls]
+
+    @classmethod
+    def get_name(cls, value: int) -> str:
+        for item in cls:
+            if item.value == value:
+                return item.name
+        raise InvalidArgument(f'Invalid value for Fromat: {value}')
+
+    @classmethod
+    def values(cls) -> List[int]:
+        return [item.value for item in cls]
+
+    @classmethod
+    def from_value(cls, value: int) -> 'Fromat':
+        for item in cls:
+            if item.value == value:
+                return item
+        raise InvalidArgument(f'Invalid value for Fromat: {value}')
---
+++ src/jsonmaestro/enums/format.pyi
@@ -0,0 +1,32 @@
+from enum import Enum
+from typing import List
+
+
+class Fromat(Enum):
+    eNone = 0
+    eJson = 1
+    eJsonWithCommnets = 2
+
+    @classmethod
+    def count(cls) -> int:
+        ...
+
+    @classmethod
+    def max(cls) -> int:
+        ...
+
+    @classmethod
+    def names(cls) -> List[str]:
+        ...
+
+    @classmethod
+    def get_name(cls, value: int) -> str:
+        ...
+
+    @classmethod
+    def values(cls) -> List[int]:
+        ...
+
+    @classmethod
+    def from_value(cls, value: int) -> 'Fromat':
+        ...
